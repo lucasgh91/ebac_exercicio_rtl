32214861482c859cf4f71ac2b79664ef08f0f519
Lucas G. <ghizzolucas@gmail.com>
2024-11-15 03:07:26 -0300

    Exercício resolvido

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    it('Deve inserir um comentário 1', () => {
         render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Comentário 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-enviar'))
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument()
+    });
+
+    it('Deve inserir um comentário 2', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Comentário 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-enviar'))
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument()
     });
 });
